a40f784f3732576b47e33601e79708c31366e9de
Cheng Li <cli99@illinois.edu>
2017-07-20 11:15:58 -0400

    local tracing example

--- zipkin-cpp/examples/simple_test/main.cpp
+++ zipkin-cpp/examples/simple_test/main.cpp
@@ -1,26 +1,39 @@
-#include <iostream>       // std::cout, std::endl
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
+#include <chrono>   // std::chrono::seconds
+#include <iostream> // std::cout, std::endl
+#include <thread>   // std::this_thread::sleep_for
 
 #include <zipkin.hpp>
 
-int main(int argc, char **argv)
-{
-  // google::InitGoogleLogging(argv[0]);
-std::unique_ptr<zipkin::HttpConf> conf(new zipkin::HttpConf("http://localhost:9411/api/v1/spans"));
-std::shared_ptr<zipkin::HttpCollector> collector(conf->create());
-std::unique_ptr<zipkin::Tracer> tracer(zipkin::Tracer::create(collector.get()));
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  const auto url = "http://127.0.0.1:9411/api/v1/spans";
+  std::unique_ptr<zipkin::HttpConf> conf(new zipkin::HttpConf(url));
+  std::unique_ptr<zipkin::HttpCollector> collector(conf->create());
+  std::unique_ptr<zipkin::Tracer> tracer(
+      zipkin::Tracer::create(collector.get()));
 
-if (collector.get()) conf.release();
-
-zipkin::Span& span = *tracer->span("encode");
-zipkin::Span::Scope scope(span);
-
-  std::cout << "countdown:\n";
-  for (int i=5; i>0; --i) {
-    std::cout << i << std::endl;
-    std::this_thread::sleep_for (std::chrono::seconds(1));
+  if (collector.get()) {
+    conf.release();
   }
-  std::cout << "Lift off!\n";
+
+  sockaddr_in addr;
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  addr.sin_port = htons(9411);
+
+  // dropdown_name is the one that shows up in the zipkin dropdown
+  const zipkin::Endpoint endpoint("dropdown_name", &addr);
+
+  for (int ii = 0; ii < 2; ii++) {
+    // trace_name is the name of the trace
+    zipkin::Span &span = *tracer->span("trace_name");
+    zipkin::Span::Scope scope(span);
+
+    span.client_send(&endpoint);
+
+    span << std::make_pair("some_tag", "0.3." + std::to_string(ii));
+    span << std::make_pair("another_tag", std::to_string(ii));
+    span << std::make_pair("something_else", "something_else");
+  }
+  collector->shutdown(std::chrono::seconds(5));
   return 0;
 }
