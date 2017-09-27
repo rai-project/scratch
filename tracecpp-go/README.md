50bf9519973a2ba2aa956424696b2ba2192f98df
Abdul Dakkak <abduld@wolfram.com>
2017-09-26 22:12:03 -0500

    add readme and use tracer

---
+++ tracecpp-go/README.md
@@ -0,0 +1,11 @@
+
+# Tracing C code via callback
+
+Run using `GODEBUG=cgocheck=0 go run main.go`
+
+## Resources
+
+
+- https://stackoverflow.com/questions/38730989/cgo-go-pointers-in-go-memory
+- https://github.com/golang/go/wiki/cgo
+- https://stackoverflow.com/questions/37157379/passing-function-pointer-to-the-c-code-using-cgo
--- tracecpp-go/main.go
+++ tracecpp-go/main.go
@@ -4,30 +4,68 @@ package main
 
 #include "stdlib.h"
 
-extern void go_callback_int(char * foo, int p1);
+extern void go_callback_int(void * data, char * foo, int p1);
 
 // normally you will have to define function or variables
 // in another separate C file to avoid the multiple definition
 // errors, however, using "static inline" is a nice workaround
 // for simple functions like this one.
-static inline void CallMyFunction(char * foo) {
-	go_callback_int(foo, 5);
+static inline void CallMyFunction(void * data, char * foo) {
+	go_callback_int(data, foo, 5);
 }
 */
 import "C"
 import (
 	"fmt"
 	"sync"
+	"time"
 	"unsafe"
+
+	context "golang.org/x/net/context"
+
+	"github.com/fatih/color"
+	"github.com/k0kubun/pp"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/rai-project/config"
+	"github.com/rai-project/logger"
+	tr "github.com/rai-project/tracer"
+	_ "github.com/rai-project/tracer/jaeger"
+	_ "github.com/rai-project/tracer/noop"
+	_ "github.com/rai-project/tracer/zipkin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	IsDebug   bool
+	IsVerbose bool
+	AppSecret string
+	CfgFile   string
+	tracer    tr.Tracer
+	log       *logrus.Entry = logrus.New().WithField("pkg", "scratch/tracepp-go")
 )
 
 //export go_callback_int
-func go_callback_int(foo *C.char, p1 C.int) {
+func go_callback_int(data unsafe.Pointer, foo *C.char, p1 C.int) {
+	ctx := (*context.Context)(data)
+	callback(*ctx, foo, p1)
+}
+
+func callback(ctx context.Context, foo *C.char, p1 C.int) {
+
+	span, ctx := tracer.StartSpanFromContext(ctx, C.GoString(foo))
+	defer span.Finish()
+
+	time.Sleep(time.Second)
+
 	fn := lookup(C.GoString(foo))
+
+	s, ctx := tracer.StartSpanFromContext(ctx, "calling fn")
+	defer s.Finish()
 	fn(p1)
 }
 
 func MyCallback(x C.int) {
+	time.Sleep(time.Duration(x) * time.Second)
 	fmt.Println("callback with", x)
 }
 
@@ -35,7 +73,13 @@ func Example() {
 	name := register("MyCallback", MyCallback)
 	cstr := C.CString(name)
 	defer C.free(unsafe.Pointer(cstr))
-	C.CallMyFunction(cstr)
+
+	ctx := context.Background()
+
+	span, ctx := tracer.StartSpanFromContext(ctx, "top level")
+	defer span.Finish()
+
+	C.CallMyFunction(unsafe.Pointer(&ctx), cstr)
 	unregister(name)
 }
 
@@ -62,6 +106,31 @@ func unregister(name string) {
 	delete(fns, name)
 }
 
+// Init reads in config file and ENV variables if set.
+func Init() {
+
+	log.Level = logrus.DebugLevel
+	config.AfterInit(func() {
+		log = logger.New().WithField("pkg", "scratch/tracepp-go")
+		tracer = tr.MustNew("tracepp")
+		pp.Println(tracer.Endpoints())
+	})
+
+	color.NoColor = false
+	opts := []config.Option{
+		config.AppName("carml"),
+		config.ColorMode(true),
+		config.DebugMode(IsDebug),
+		config.VerboseMode(IsVerbose),
+	}
+	if c, err := homedir.Expand(CfgFile); err == nil {
+		CfgFile = c
+	}
+	config.Init(opts...)
+}
+
 func main() {
+	Init()
+	defer tracer.Close()
 	Example()
 }
