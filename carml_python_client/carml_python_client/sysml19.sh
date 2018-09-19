a8cd41f78fa6b73b6231c804a4711aece4e24b70
Abdul Dakkak <abduld@wolfram.com>
2018-09-19 10:36:09 -0500

    updates

--- carml_python_client/carml_python_client/sysml19.sh
+++ carml_python_client/carml_python_client/sysml19.sh
@@ -9,9 +9,8 @@ python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29
 python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="STEP_TRACE" --framework_name="Caffe2" --framework_version="0.8.1" --batch_size 64
 python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="FULL_TRACE" --framework_name="Caffe2" --framework_version="0.8.1" --batch_size 64
 
-python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="STEP_TRACE" --framework_name="TensorFlow" --framework_version="1.4" --batch_size 64
-python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="FULL_TRACE" --framework_name="TensorFlow" --framework_version="1.4" --batch_size 64
-
 python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="STEP_TRACE" --framework_name="TensorRT" --framework_version="2.1.2" --batch_size 64
 python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="FULL_TRACE" --framework_name="TensorRT" --framework_version="2.1.2" --batch_size 64
 
+python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="STEP_TRACE" --framework_name="TensorFlow" --framework_version="1.4" --batch_size 64
+python cli.py --carml_url http://34.229.181.50:8088/ --trace_url http://52.91.29.125 --urls example.txt --trace_level="FULL_TRACE" --framework_name="TensorFlow" --framework_version="1.4" --batch_size 64
